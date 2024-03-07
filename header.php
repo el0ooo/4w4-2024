2976dddbe9e8dc13f017e9f02ea50d2095fc587e
el0ooo <eloichayer@gmail.com>
2024-03-07 15:57:42 -0500

    s5c2 - ajout d'un menu burger, manque animation dessus

--- header.php
+++ header.php
@@ -12,6 +12,10 @@
 <body>
     <div id="menu">
         <header class="entete__menu">
+            <label id="burger" for="chk_burger">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </label>
+            <input id="chk_burger" type="checkbox">
             <?php wp_nav_menu(array("container" => "nav"));?>
         </header>
     </div>
