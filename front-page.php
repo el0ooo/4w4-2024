29e00986c16f65a0023addffb0936706a888fc95
el0ooo <eloichayer@gmail.com>
2024-05-13 11:15:56 -0400

    s13c2 - fin des gabarits

--- front-page.php
+++ front-page.php
@@ -18,14 +18,12 @@
             <div id="populaire" class="section__cours">
                 <?php if(have_posts()):
                         while(have_posts()): the_post();      
+                        $maCategorie = "carte";
+                        if(in_category("galerie")){
+                            $maCategorie = "galerie";
+                        }
+                        get_template_part('gabarit/categorie', $maCategorie);
                 ?>
-                        <div class="carte bck-primaire-100"> 
-                            <h5><?php the_title(); ?></h5>
-                            <p><?php echo wp_trim_words(get_the_content(), 10, "...");?></p>
-                            <p>Proche de <?php the_field("ville_avoisinante");?></p>
-                            <p class="categori__carte"><?php the_category();?></p>
-                            <p><a href= "<?php echo get_permalink()?>">Voir la suite</a></p>
-                        </div>
                     <?php endwhile;?>
                 <?php endif;?>
             </div>
--- gabarit/categorie-galerie.php
+++ gabarit/categorie-galerie.php
@@ -2,5 +2,6 @@
 // gabarit permettant d'afficher une galerie
 ?>
 <div class="galeire">
+    <?php the_title(); ?>
     <?php the_content(); ?>
 </div>
