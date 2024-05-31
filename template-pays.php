4dc74a13ed3e2d0ad2ecfbe2ad61debd1f829fad
el0ooo <eloichayer@gmail.com>
2024-05-31 14:38:07 -0400

    ajout du template

---
+++ template-pays.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template name: Pays
+ *
+ */
+?>
+<?php get_header(); ?>
+<main class="site__main">
+<?php 
+    if ( have_posts() ) : the_post(); 
+?>
+<h1><?= get_the_title(); ?></h1>
+<?php the_content();?>
+<?php
+    endif;
+    add_shortcode('EC_pays', 'creation_destinations');
+?>
+</main>
+<?php
+get_footer();
